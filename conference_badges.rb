edcbc2ccee7b297898d6e4387ae6977aca3aa6c4
Jorge Ramirez <joramirez96@gmail.com>
2020-06-12 18:14:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,6 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
-  attendees["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(attendees, badge)
+  ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 end
